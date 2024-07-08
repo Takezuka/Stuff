e315e16929b2b439933153abc4eb5834f8846f36
EARZU CHAN <63433558+EarzuChan@users.noreply.github.com>
2024-07-08 21:55:26 +0800

    Delete docs directory

--- docs/.vuepress/client.js
+++
@@ -1,8 +0,0 @@
-import {defineClientConfig} from 'vuepress/client'
-import AudioPlayer from "./components/AudioPlayer.vue"
-
-export default defineClientConfig({
-    enhance({app}) {
-        app.component('AudioPlayer', AudioPlayer)
-    }
-})
